89cdc5970561f1542b3e1d1473c72d4d5d0536f5
Dinesh Parmar <dparmar1999.dp@gmail.com>
2021-05-15 17:24:28 +0530

    First commit

---
+++ Tasker/main.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+
+URL_LINK="URL_LINK_HERE"
+
+body = requests.get(URL_LINK)
+
+soup = BeautifulSoup(body.text, 'html.parser')
+
+list_of_images = soup.find_all('img')
+#print(list_of_images)
+source = [img['src'] for img in list_of_images]
+
+imageCount=0
+
+for i,link in enumerate(source):
+    with open(f"images/images{i+1}.jpg","wb+") as f:
+        if ".svg" not in link:
+            #print(link)
+            if 'http' not in link:
+                link = '{}{}'.format(URL_LINK, link)
+            response = requests.get(link)
+            imageCount+=1
+            f.write(response.content)
+
+print("{} images were found".format(imageCount))
+
